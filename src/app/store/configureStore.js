d2a57e97f9f8302250bbe485c3a8598f4e7f4992
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-09-28 11:37:03 -0500

    removed redux tools extension for now

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,16 +1,9 @@
-import { createStore, compose, applyMiddleware } from "redux"
-import rootReducer from "../reducers/rootReducer"
+import { createStore, applyMiddleware } from "redux"
+import rootReducer from "app/reducers/rootReducer"
 import thunk from "redux-thunk"
 
 const configureStore = () => {
-  const store = createStore(
-    rootReducer,
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__(),
-    ),
-  )
+  const store = createStore(rootReducer, applyMiddleware(thunk))
   return store
 }
 
